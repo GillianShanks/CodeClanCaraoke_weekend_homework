7ce13bb911112c60662532eaecca0a3bbf7b9311
Gillian Shanks <GillianShanks88@gmail.com>
2019-07-26 15:19:19 +0100

    Adds wallet to guest, passes test

--- specs/guest_spec.rb
+++ specs/guest_spec.rb
@@ -4,10 +4,14 @@ require_relative ("../Guest.rb")
 
 class GuestTest < MiniTest::Test
   def setup()
-    @guest1 = Guest.new("Louise")
+    @guest1 = Guest.new("Louise", 100)
   end
 
   def test_guest_has_name()
     assert_equal("Louise", @guest1.name)
+  end
+
+  def test_guest_has_money()
+    assert_equal(100, @guest1.wallet)
   end
 end
