db99044b097b6ec1c975a5d9dc579b0b7ba30c4a
Gillian Shanks <GillianShanks88@gmail.com>
2019-07-27 09:58:32 +0100

    adds function to reduce all room fees that are listed in the Bar instance - used enumeration.

--- Bar.rb
+++ Bar.rb
@@ -1,6 +1,21 @@
+require("pry-byebug")
 class Bar
   attr_reader :total
-  def initialize()
+  def initialize(rooms)
+    @rooms = rooms
     @total = 0
   end
+
+  def add(money)
+    return @total += money
+  end
+
+  def room_count()
+    return @rooms.count()
+  end
+
+  def add_room_fees()
+    @total = @rooms.map{|room| room.till}.reduce{|total, till| total + till}
+  end
+
 end
--- specs/bar_spec.rb
+++ specs/bar_spec.rb
@@ -1,14 +1,35 @@
 require ("minitest/autorun")
 require ("minitest/rg")
 require_relative ("../Bar.rb")
+require_relative ("../Room.rb")
 
 class BarTest < MiniTest::Test
 
   def setup()
-    @bar = Bar.new()
+    @room1 = Room.new("90s Room", 5)
+    @room2 = Room.new("80s Room", 10)
+    rooms = [@room1, @room2]
+    @bar = Bar.new(rooms)
+
   end
 
   def test_total_starts_at_zero()
     assert_equal(0, @bar.total)
+  end
+
+  def test_add_to_total()
+    @bar.add(10)
+    assert_equal(10, @bar.total)
+  end
+
+  def test_room_count()
+    assert_equal(2, @bar.room_count())
+  end
+
+  def test_add_rooms_to_total()
+    @room1.add_fee(5)
+    @room2.add_fee(10)
+    @bar.add_room_fees()
+    assert_equal(15, @bar.total)
   end
 end
